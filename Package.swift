73b2c2db06455b6d355134d14a7011ab7b545c94
thienNH0 <147373819+thienNH0@users.noreply.github.com>
2024-08-27 16:46:52 +0700

    chore: change swift package name to SkyMavis-Waypoint (#8)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "waypoint",
+    name: "SkyMavis-Waypoint",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
